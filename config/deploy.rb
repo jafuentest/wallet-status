aec46ed836c945c20b132367eeb13f1d05d6b2b6
Juan Fuentes <jn.fuentest@gmail.com>
2022-05-15 02:08:37 -0400

    Use credentials.yml on production

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, '/home/ec2-user/wallet_status'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w[config/master.key]
+set :linked_files, %w[config/master.key config/credentials.yml]
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'vendor', 'storage'
