2b229a1f123bf25c2ef6cece545be8fdd87b1f86
Juan Fuentes <jn.fuentest@gmail.com>
2022-06-05 13:54:16 -0400

    Redirect when request fails csrf check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActionController::InvalidAuthenticityToken, with: :redirect_to_referer_or_root
+
   def not_found
     Rails.logger.info "Got not_found for #{request.method} #{request.path}"
     return head :not_found if request.xhr? || !request.format.html?
 
     render file: 'public/404.html'
+  end
+
+  private
+
+  def current_action
+    matched_route = Rails.application.routes.recognize_path(request.env['PATH_INFO'])
+    matched_route[:action]
+  end
+
+  def redirect_to_referer_or_root
+    return not_found if current_action == 'not_found'
+
+    flash[:notice] = 'Please try again.'
+    redirect_to request.referer.present? ? request.referer : root_path
   end
 end
