7c33257f169bf09fa57a47a46320ef3982f9b073
Juan Fuentest <jn.fuentest@gmail.com>
2022-06-22 23:05:52 -0400

    Update Fetch Transations run at time (#29)
    * Set recurring job to every day at 6:00 am/pm

--- app/services/recurring_tasks/fetch_transations.rb
+++ app/services/recurring_tasks/fetch_transations.rb
@@ -1,8 +1,8 @@
 class RecurringTasks::FetchTransations
   include Delayed::RecurringJob
 
-  run_every 12.hours
-  run_at '12:00', '0:00'
+  run_every 1.day
+  run_at '18:00', '6:00'
 
   def perform
     Rails.logger.info 'Creating fetch transations jobs'
