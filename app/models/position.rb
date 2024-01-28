a2234d9240364a2bdce2bb9773524467429c47c4
Juan Fuentes <jn.fuentest@gmail.com>
2024-01-28 20:38:26 +0100

    Add database schema to models

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: positions
+#
+#  id         :bigint           not null, primary key
+#  wallet_id  :bigint           not null
+#  sub_wallet :string
+#  cost_basis :decimal(, )
+#  amount     :decimal(, )      not null
+#  symbol     :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Position < ApplicationRecord
   belongs_to :wallet
 
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: transactions
+#
+#  id              :bigint           not null, primary key
+#  wallet_id       :bigint           not null
+#  from_asset      :string
+#  from_amount     :decimal(, )
+#  from_cost_basis :decimal(, )
+#  to_asset        :string
+#  to_amount       :decimal(, )
+#  to_cost_basis   :decimal(, )
+#  fee_asset       :string
+#  fee_amount      :decimal(, )
+#  fee_cost_basis  :decimal(, )
+#  order_id        :string
+#  order_type      :string           not null
+#  timestamp       :datetime         not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class Transaction < ApplicationRecord
   belongs_to :wallet
 
