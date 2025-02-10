d48d720bc6e67a6d41cdef777671750c6aebc305
Juan Fuentes <jn.fuentest@gmail.com>
2025-02-11 00:22:47 +0100

    Fix job parent class

--- app/jobs/fetch_spot_trades_job.rb
+++ app/jobs/fetch_spot_trades_job.rb
@@ -1,4 +1,4 @@
-class FetchSpotTradesJob < ActiveJob::Base
+class FetchSpotTradesJob < ApplicationJob
   def perform(wallet_id, pair)
     wallet = Wallet.find(wallet_id)
     TransactionFetchers::Binance::Spot.new(wallet).fetch_pair(pair)
