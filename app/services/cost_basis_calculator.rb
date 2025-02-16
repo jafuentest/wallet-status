a794d27462327cefa689e11e7a649720e430e879
Juan Fuentes <jn.fuentest@gmail.com>
2025-02-16 03:45:13 +0100

    Finish cost basis calc implementation

--- app/services/cost_basis_calculator.rb
+++ app/services/cost_basis_calculator.rb
@@ -6,31 +6,45 @@ class CostBasisCalculator
   end
 
   def calculate
-    @user.transactions.order(:timestamp)
-      .each { |transaction| process_transaction(transaction) }
+    @user.transactions.order(:timestamp).each do |transaction|
+      ActiveRecord::Base.transaction do
+        process_transaction(transaction)
+      end
+    end
   end
 
   private
 
   def process_transaction(transaction)
     asset_changes(transaction).each do |change|
-      current_cost_basis = asset_cost_basis_for(change[:asset])
-      new_cost_basis = @strategy.calculate(change[:asset], change[:amount], current_cost_basis)
-      # Update cache
-      # Insert in cost basis changes table
+      new_cost_basis = @strategy.calculate(
+        change[:asset],
+        change[:amount],
+        asset_cost_basis_for(change[:asset])
+      )
+
+      update_cost_basis(new_cost_basis)
     end
-    # Persist cache to cost bases table
+  end
+
+  def asset_changes(transaction)
+    [
+      { amount: transaction.to_amount, asset: transaction.to_asset },
+      { amount: -transaction.fee_amount, asset: transaction.fee_asset },
+      { amount: -transaction.from_amount, asset: transaction.from_asset },
+    ].reject { |e| e[:asset].nil? }
   end
 
   def asset_cost_basis_for(asset)
     @asset_cost_basis_cache[asset] ||= CostBasis.find_or_initialize_by(user: @user, asset: asset)
   end
 
-  def asset_changes(transaction)
-    [
-      { amount: transaction.from_amount, asset: transaction.from_asset },
-      { amount: transaction.to_amount, asset: transaction.to_asset },
-      { amount: transaction.fee_amount, asset: transaction.fee_asset },
-    ].reject { |e| e[:asset].nil? }
+  def update_cost_basis(new_cost_basis, amount)
+    new_cost_basis.save!
+
+    cost_basis_object = asset_cost_basis_for(new_cost_basis.asset)
+    cost_basis_object.total_amount += amount
+    cost_basis_object.cost_basis += new_cost_basis.amount
+    cost_basis_object.save!
   end
 end
